c9b220bd505d2244eab86bd1e9784f3dd7b7dca1
Simon946 <56819218+Simon946@users.noreply.github.com>
2025-01-03 14:09:15 +0100

    Update scheduler.py
    this now also works, the nodes are separated per connection
    
    Signed-off-by: Simon946 <56819218+Simon946@users.noreply.github.com>

--- external_scheduler/scheduler.py
+++ external_scheduler/scheduler.py
@@ -2,6 +2,7 @@ import socket
 import selectors
 import struct
 from enum import Enum, IntEnum
+import time
 
 class API_CODES(IntEnum):
     ADD_NODE = 0
@@ -13,6 +14,7 @@ PORT = 44444
 
 sel = selectors.DefaultSelector()
 # Should probably separate nodes by which client added them? Something to check out later
+node_info = {} #Key: connection, Value: pair of (nodes, task queues) 
 nodes = {} # Key: nodeID; Value: Dict {Key: ascii resource key; Value: resource value}
 # Currently just increments task_counter and uses that as taskID. Therefore, cannot handle removing tasks from the queue, because of which some queues can get increasingly long while others are empty
 task_queue = {} # Key: nodeID; Value: List{taskID} 
@@ -26,10 +28,12 @@ def decode_api_resources(bytestr):
         val = bytestr[:8]
         bytestr = bytestr[8:]
         resources[key] = struct.unpack('<d', val)[0]
+    print("api resources:", resources)
     return resources
 
 # Finds node with shortest queue that matches task's resource requirements
-def find_optimal_node(resources):
+def find_optimal_node(client_sock, resources):
+    (nodes, task_queue) = node_info[client_sock.fileno]
     possible_nodes = []
     available = True
     for nodeID, avail_resources in nodes.items():
@@ -55,25 +59,30 @@ def find_optimal_node(resources):
     return 0, optimal_node
 
 def handle_message(client_sock, message):
+    (nodes, task_queue) = node_info[client_sock.fileno]
     global task_counter
     api_code = message[0]
     message = message[1:]
     if api_code == API_CODES.ADD_NODE:
-        nodeID = int.from_bytes(message[:8], byteorder='little')
+        
+        nodeID = int.from_bytes(message[:8], byteorder='little', signed=True)
+        print("add node message received, node id:", message[:8].hex())
         resources = decode_api_resources(message[8:])
         nodes[nodeID] = resources
         task_queue[nodeID] = []
         return b'\x00'
     elif api_code == API_CODES.REMOVE_NODE:
-        nodeID = int.from_bytes(message[:8], byteorder='little')
+        nodeID = int.from_bytes(message[:8], byteorder='little', signed=True)
+        print("remove node message received, node id:", message[:8].hex())
+        
         if not nodeID in nodes:
-            return b'\x01'
-        del nodes[nodeID]
+            return b'\x00'
         del task_queue[nodeID]
         return b'\x00'
     elif api_code == API_CODES.SCHEDULE:
+        print("schedule message received")
         resources = decode_api_resources(message)
-        code, optimal_node = find_optimal_node(resources)
+        code, optimal_node = find_optimal_node(client_sock, resources)
         if code == 1:
             return b'\x01'
         task_queue[optimal_node].append(task_counter)
@@ -89,6 +98,7 @@ def accept_connection(server_sock):
     print(f"Client connected: {addr}")
     client_sock.setblocking(False)
     sel.register(client_sock, selectors.EVENT_READ, read_client)
+    node_info[client_sock.fileno] = ({}, {})
 
 def read_client(client_sock):
     # Read data from client socket
@@ -106,17 +116,27 @@ def read_client(client_sock):
         msg_len = int.from_bytes(msg[:8], byteorder='little', signed=True)
         msg = b''
         while len(msg) < msg_len:
-            tmp = client_sock.recv(msg_len - len(msg))
+            try:
+                tmp = client_sock.recv(msg_len - len(msg))
+                
+            except (BlockingIOError):
+                print("...");
+                time.sleep(1)
+                continue
+            
             if not tmp:
                 close_connection(client_sock)
                 return
             msg += tmp    
 
         # Process message
+        print("input message:", msg)
         ret_message = handle_message(client_sock, msg)
-
+        print("response message:", ret_message)
         # Send message back
         ret_msg_len = len(ret_message)
+        print("replying with message:", ret_message.hex(), "length: ", ret_msg_len)
+        print("length again:", ret_msg_len.to_bytes(8, byteorder='little').hex())
         client_sock.send(ret_msg_len.to_bytes(8, byteorder='little') + ret_message)        
     except ConnectionResetError:
         close_connection(client_sock)
@@ -126,6 +146,7 @@ def close_connection(client_sock):
     print("Closing connection.")
     sel.unregister(client_sock)
     client_sock.close()
+    del node_info[client_sock.fileno]
 
 def start_server(host, port):
     # Start TCP server
